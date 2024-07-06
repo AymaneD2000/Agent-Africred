1d9097c4b2378948b2a61fdd8b7ce83b3642b925
Babry <babrigaledou7@gmail.com>
2024-07-06 12:04:31 +0000

    recouvre

---
+++ lib/screens/recouvrement/recouvrement.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Recouvrement extends StatefulWidget {
+  const Recouvrement({super.key});
+
+  @override
+  State<Recouvrement> createState() => _RecouvrementState();
+}
+
+class _RecouvrementState extends State<Recouvrement> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(),
+    );
+  }
+}
